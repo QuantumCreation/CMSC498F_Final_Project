962f0a7899df07b2c8ad2790b7add7a6e6d5ccb9
myack99 <99228931+myack99@users.noreply.github.com>
2022-04-26 00:42:26 -0400

    Update Lock.cs

--- ScriptsFolder/Lock.cs
+++ ScriptsFolder/Lock.cs
@@ -2,6 +2,7 @@
 // Attach it to the lock
 // Assign the main camera GO to the 'GameObject camera' in Unity
 // Assign the corresponding key GO to the 'GameObject key' in Unity
+// Asign the door this button controls GO to the 'GameObject door' in Unity
 
 // When the lock is interacted with, if it can find the GO associated with 'key' then it will not unlock
 
@@ -15,6 +16,7 @@ public class Lock : MonoBehaviour
 
     public GameObject camera;
     public GameObject key;
+    public GameObject door;
 
     void Update()
     {
@@ -28,6 +30,7 @@ public class Lock : MonoBehaviour
                     print("unlock?");
                     if (!(key.gameObject)) {
                         print("UNLOCKED!");
+                        Destroy(door.gameObject);
                     }
                 }
             }
