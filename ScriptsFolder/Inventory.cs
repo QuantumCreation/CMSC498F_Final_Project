5c7748fd95c3849a2bcdf2272c2f3f9005796c02
myack99 <99228931+myack99@users.noreply.github.com>
2022-04-26 00:30:57 -0400

    Add files via upload

---
+++ ScriptsFolder/Inventory.cs
@@ -0,0 +1,41 @@
+// This script is for processing the player's inventory
+// Attach it to the main camera GO
+// Assign a key to each 'public GameObject key[]' in Unity
+
+// Inventory currently cannot be viewed
+    // GO is destroyed and this means that when the lock is interacted with for destroyed key it will open
+
+
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inventory : MonoBehaviour
+{
+
+    public GameObject key1;
+    public GameObject key2;
+
+    void Update()
+    {
+
+        if (Input.GetKeyDown(KeyCode.H)){
+            print("H");
+            RaycastHit result;
+            if (Physics.Raycast(this.gameObject.transform.position,this.gameObject.transform.forward,out result, Mathf.Infinity)){
+                print("My ray hit: "+result.collider.gameObject.name);
+
+                if (result.collider.gameObject == key1) {
+                    print("Key 1 collected");
+                    Destroy(result.collider.gameObject);
+                } 
+
+                if (result.collider.gameObject == key2) {
+                    print("Key 2 collected");
+                    Destroy(result.collider.gameObject);
+                } 
+            }
+        }
+    }
+}
---
+++ ScriptsFolder/Lock.cs
@@ -0,0 +1,36 @@
+// This script is for unlocking locks with colected keys
+// Attach it to the lock
+// Assign the main camera GO to the 'GameObject camera' in Unity
+// Assign the corresponding key GO to the 'GameObject key' in Unity
+
+// When the lock is interacted with, if it can find the GO associated with 'key' then it will not unlock
+
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lock : MonoBehaviour
+{
+
+    public GameObject camera;
+    public GameObject key;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.U)){
+            print("U");
+            RaycastHit result;
+            if (Physics.Raycast(camera.gameObject.transform.position,camera.gameObject.transform.forward,out result, Mathf.Infinity)){
+                print("My ray hit: "+result.collider.gameObject.name);
+
+                if (result.collider.gameObject == this.gameObject) {
+                    print("unlock?");
+                    if (!(key.gameObject)) {
+                        print("UNLOCKED!");
+                    }
+                }
+            }
+        }
+    }
+}
