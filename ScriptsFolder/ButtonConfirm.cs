9ac8d8aafbfb07017733b8857828b6c00cec4952
myack99 <99228931+myack99@users.noreply.github.com>
2022-05-02 21:43:07 -0400

    Update ButtonConfirm.cs

--- ScriptsFolder/ButtonConfirm.cs
+++ ScriptsFolder/ButtonConfirm.cs
@@ -1,9 +1,9 @@
-// This script is for the button to confirm a numerical code entry
+// This script is for the button to confirm a NUMERICAL CODE entry
 // Attach it to the button/lever that is used to confirm code
 // Assign the main camera GO to the 'GameObject camera' in Unity
-// Assign the first number text GO to the 'GameObject numberCode1' in Unity
-// Assign the second number text GO to the 'GameObject numberCode2' in Unity
 // Asign the door this button controls GO to the 'GameObject door' in Unity
+// Assign the first number text GO or gear GO to the 'GameObject numberCode1' in Unity
+// Assign the second number text GO or gear GO to the 'GameObject numberCode2' in Unity
 // Assign a number 0-9 to the 'GameObject value1' in Unity
 // Assign a number 0-9 to the 'GameObject value2' in Unity
 // If you want to add another number:
@@ -21,9 +21,9 @@ public class ButtonConfirm : MonoBehaviour
 {
 
     public GameObject camera;
+    public GameObject door;
     public GameObject numberCode1;
     public GameObject numberCode2;
-    public GameObject door;
     public int value1;
     public int value2;
 
@@ -37,8 +37,8 @@ public class ButtonConfirm : MonoBehaviour
 
                 if (result.collider.gameObject == this.gameObject) {
                     print("confirm?");
-                    if (numberCode1.gameObject.transform.GetComponent<TextMesh>().text == value1.ToString()) {
-                        if (numberCode2.gameObject.transform.GetComponent<TextMesh>().text == value2.ToString()) {
+                    if (numberCode1.GetComponent<ChangeValue>().value == value1) {
+                        if (numberCode2.GetComponent<ChangeValue>().value == value2) {
                             // add another if statement here for more numbers
                             print("UNLOCKED!");
                             Destroy(door.gameObject);
